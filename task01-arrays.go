bd9760469d0df5617a5902859da4aa51afae3e27
Шалыго Сергей Михайлович <Shalygo.S.Mi@sberbank.ru>
2022-05-09 22:17:43 +0300

    третья попытка

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,11 +1,11 @@
 package homework
 
-func average(input [15]float32, N int64) (result float32) {
+func average(input [15]float32) (result float32) {
 
 	result = 0.0
 	for _, v := range input {
 		result += v
 	}
 
-	return result / float32(N)
+	return result / float32(len(input))
 }
